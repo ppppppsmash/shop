dd1df59b075a1980b26f74ba8c99153a2231c18a
ppppppsmash <pppppp.work@gmail.com>
2023-09-09 23:14:17 +0900

    add: 検索欄 & 画像

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,5 +1,7 @@
 <script setup>
-let isAccountMenu = ref(true)
+let isAccountMenu = ref(false)
+let isSearching = ref(true)
+let searchItem = ref('')
 </script>
 
 <template>
@@ -70,9 +72,77 @@ let isAccountMenu = ref(true)
                 </NuxtLink>
               </div>
             </div>
+
+            <div class="border-b" />
+            <ul class="bg-white">
+              <li
+                @click="navigateTo('/orders')"
+                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+              >
+                My Orders
+              </li>
+              <li
+                v-if="true"
+                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+              >
+                Sign out
+              </li>
+            </ul>
           </div>
         </li>
       </ul>
+    </div>
+
+    <div
+      id="MainHeader"
+      class="flex items-center w-full bg-white"
+    >
+      <div class="flex lg:justify-start justify-between gap-10 max-w-[1150px]
+        w-full px-3 py-5 mx-auto"
+      >
+        <NuxtLink
+          to="/"
+          class="min-w-[170px]"
+        >
+          <img 
+            width="170"
+            src="/AliExpress-logo.png"
+          />
+          <!-- <Icon name="game-icons:death-zone" size="20" /> -->
+        </NuxtLink>
+
+        <div class="max-w-[700px] w-full md:block hidden">
+          <div class="relative">
+            <div class="flex items-center border-2 border-[#FF4646] rounded-md w-full">
+              <input
+                class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
+                placeholder="text text"
+                type="text"
+                v-model="searchItem"
+              />
+              <Icon
+                v-if="isSearching"
+                name="eos-icons:loading"
+                size="25"
+                class="mr-2"
+              />
+              <button class="flex items-center h-[100%] p-1.5 px-2 bg-[#FF4646]">
+                <Icon
+                  name="ph:magnifying-glass"
+                  size="20"
+                  color="#FFFFFF"
+                />
+              </button>
+            </div>
+
+            <div class="absolute bg-white max-w-[700px] h-auto w-full">
+              <div class="p-1">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>
