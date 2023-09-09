8a5b65541d7c9549a036a1bb9fbfb342b5832e8e
ppppppsmash <pppppp.work@gmail.com>
2023-09-09 22:43:13 +0900

    add: MainLayout & header

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,7 +1,79 @@
 <script setup>
-
+let isAccountMenu = ref(true)
 </script>
 
 <template>
+  <div
+    id="MainLayout"
+    class="w-full fixed z-50"
+  >
+    <div
+      id="TopMenu"
+      class="w-full bg-[#FAFAFA] border-b md:block hidden"
+    >
+      <ul
+        class="flex items-center justify-end text-xs text-[#333333] font-light px-2
+          h-10 bg-[#FAFAFA] max-w-[1200px]"
+      >
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Sell on Kurosawa's Project
+        </li>
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Cookie Preferences
+        </li>
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Help
+        </li>
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Buyer Protection
+        </li>
+        <li
+          class="px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          <Icon name="ic:sharp-install-mobile" size="17" />
+          App
+        </li>
+        <li
+          @mouseenter="isAccountMenu = true"
+          @mouseleave="isAccountMenu = false"
+          class="relative flex items-center px-2.5 hover:text-[#FF4646] h-full cursor-pointer"
+          :class="isAccountMenu ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0, 0, 0, .3)]' : 'border border-[#FAFAFA]'"
+        >
+          <Icon name="ph:user-thin" size="17" />
+          Account
+          <Icon name="mdi:chevron-down" size="15" class="ml-5" />
 
+          <div
+            id="AccountMenu"
+            v-if="isAccountMenu"
+            class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
+          >
+            <div v-if="true">
+              <div class="text-semibold text-[15px] my-4 px-3">
+                Kurosawa's Project!
+              </div>
+
+              <div class="flex items-center gap-1 px-3 mb-3">
+                <NuxtLink
+                  to="/auth"
+                  class="bg-[#FF4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2"
+                >
+                  Login / Register
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <script setup>
-
+import MainLayout from '~/layouts/MainLayout.vue'
 </script>
 
 <template>
-  <div class="bg-red-100">
-    テスト
-  </div>
+  <MainLayout>
+    <div
+      id="IndexPage"
+      class="mt-4 max-w-[1200px] mx-auto px-2"
+    >
+      IndexPage
+    </div>
+  </MainLayout>
 </template>
