068a44dbf6442388567293803f9770c11646f571
ppppppsmash <pppppp.work@gmail.com>
2023-09-10 22:15:44 +0900

    add: Footer & Loading コンポーネント

---
+++ components/Footer.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+    <div class="max-w-[1200px] mx-auto px-4">
+      <div class="md:flex gap-4 justify-between w-full">
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">
+            Help
+          </div>
+          <div class="text-[#999999] text-sm">
+            Help Center
+          </div>
+        </div>
+
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">
+            AliExpress Multi-Language Sites
+          </div>
+          <div class="text-[#999999] text-sm">
+            Portuguese, Spanish, French, German, Italian, Dutch, Turkish, Japanese, Korean
+          </div>
+        </div>
+      </div>
+
+      <div class="md:flex gap-4 justify-between w-full">
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">
+            Browse By Category
+          </div>
+          <div class="text-[#999999] text-sm">
+            All Popular, Product, Promotion, Low Price, Great Value
+          </div>
+        </div>
+
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">
+            XXX Group
+          </div>
+          <div class="text-[#999999] text-sm">
+            XXX Group Website, AliExpress, xxx, xxx
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="FooterSub" class="w-full bg-[#333333] text-sm text-gray-400">
+    <div class="max-w-[1200px] mx-auto p-4">
+      Intellectual Property Protection - Privacy Policy - Sitemap - Terms of Use - xxx
+    </div>
+  </div>
+</template>
