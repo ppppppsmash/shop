e3a6c9c4adc121bd9da0fd813300d93dccc2f2b7
ppppppsmash <pppppp.work@gmail.com>
2023-09-10 23:08:46 +0900

    add: product コンポーネント & ダミーデータとの連携

---
+++ components/ProductComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps(['product'])
+const { product } = toRefs(props)
+
+const priceComputed = computed(() => {
+  return product.value.price / 100
+})
+
+const oldPriceComputed = computed(() => {
+  let res = (product.value.price + (product.value.price / 20))
+  return res.toFixed(2)
+})
+</script>
+
+<template>
+  <div
+    :id="`ProductComponents${product.id}`"
+    class="bg-white inline-block rounded hover:shadow-[0_0_10px_3px_rgba(0, 0, 0, 0.15)] cursor-pointer"
+  >
+    <NuxtLink :to="`/item/${product.id}`">
+      <img
+        class="rounded-t"
+        :src="product.url"
+      />
+    </NuxtLink>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -12,21 +12,21 @@ const products = [
     id: 2,
     title: 'Title 2',
     description: 'This is a production',
-    url: 'https://picsum.photos/id/7/800/800',
+    url: 'https://picsum.photos/id/1/800/800',
     price: 100
   },
   {
     id: 3,
     title: 'Title 3',
     description: 'This is a production',
-    url: 'https://picsum.photos/id/7/800/800',
+    url: 'https://picsum.photos/id/9/800/800',
     price: 100
   },
   {
     id: 4,
     title: 'Title 4',
     description: 'This is a production',
-    url: 'https://picsum.photos/id/7/800/800',
+    url: 'https://picsum.photos/id/10/800/800',
     price: 100
   }
 ]
@@ -39,7 +39,9 @@ const products = [
       class="mt-4 max-w-[1200px] mx-auto px-2"
     >
       <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
-        
+        <div v-if="products" v-for="product in products" :key="product">
+          <ProductComponent :product="product" />
+        </div>
       </div>
     </div>
   </MainLayout>
