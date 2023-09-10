4cb0250807648059828796e37662f33cb786a9a5
ppppppsmash <pppppp.work@gmail.com>
2023-09-10 22:59:10 +0900

    mod: トップページ 改修

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,35 @@
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue'
+const products = [
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/7/800/800',
+    price: 100
+  },
+  {
+    id: 2,
+    title: 'Title 2',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/7/800/800',
+    price: 100
+  },
+  {
+    id: 3,
+    title: 'Title 3',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/7/800/800',
+    price: 100
+  },
+  {
+    id: 4,
+    title: 'Title 4',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/7/800/800',
+    price: 100
+  }
+]
 </script>
 
 <template>
@@ -8,7 +38,9 @@ import MainLayout from '~/layouts/MainLayout.vue'
       id="IndexPage"
       class="mt-4 max-w-[1200px] mx-auto px-2"
     >
-      IndexPage
+      <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+        
+      </div>
     </div>
   </MainLayout>
 </template>
