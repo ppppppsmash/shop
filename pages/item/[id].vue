d79f3a4c86198ebd28517ee362bec258914e0ef8
ppppppsmash <pppppp.work@gmail.com>
2023-09-11 22:04:46 +0900

    mod: 詳細markup

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -10,6 +10,10 @@ onMounted(() => {
   })
 })
 
+const priceComputed = computed(() => {
+  return '26.40'
+})
+
 const images = ref([
   '',
   'https://picsum.photos/id/212/800/800',
@@ -53,6 +57,57 @@ const images = ref([
             <p class="mb-2">Title </p>
             <p class="font-light text-[12px] mb-2">Description Section</p>
           </div>
+
+          <div class="flex items-center pt-1.5">
+            <span class="h-4 min-w-4 rounded-full p-0.5 bg-[#FFD000] mr-2">
+              <Icon name="material-symbols:star-rounded" class="-mt-[17px]" size="12" />
+            </span>
+            <p class="text-[#FF5353]">Extra 5% off</p>
+          </div>
+
+          <div class="flex items-center justify-start my-2">
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <span class="text-[13px] font-light ml-2">
+              5 213 Reviews 1,000+ orders
+            </span>
+          </div>
+
+          <div class="border-b" />
+
+          <div class="flex items-center justify-start gap-2 my-2">
+            <div class="text-xl font-bold">
+              $ {{ priceComputed }}
+            </div>
+            <span class="bg-[#F5F5F5] border text-[#C08562] text-[9px]
+              font-semibold px-1.5 rounded-sm"
+            >
+              70% off
+            </span>
+          </div>
+
+          <p class="text-[#009A66] text-xs font-semibold pt-1">
+            Free 11-day delivery over 8.28
+          </p>
+
+          <p class="text-[#009A66] text-xs font-semibold pt-1">
+            Free shipping
+          </p>
+
+          <div class="py-2" />
+
+          <button
+            @click=""
+            :disabled="isInCart"
+            class="px-6 py-2 rounded-lg text-white text-lg font-semibold
+              bg-gradient-to-r from-[#FF851A] to-[#FFAC2C]"
+          >
+          
+          </button>
+
         </div>
 
       </div>
