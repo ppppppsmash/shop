6fc849304f84533d80ba4dae352d6eb83af0bf6b
ppppppsmash <pppppp.work@gmail.com>
2023-09-11 22:32:39 +0900

    mod: shoppingcartページ

--- pages/shoppingcart.vue
+++ pages/shoppingcart.vue
@@ -1,11 +1,56 @@
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue'
+
+const products = [
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/7/800/800',
+    price: 1000
+  },
+  {
+    id: 2,
+    title: 'Title 2',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/1/800/800',
+    price: 2000
+  },
+  {
+    id: 3,
+    title: 'Title 3',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/9/800/800',
+    price: 3000
+  },
+  {
+    id: 4,
+    title: 'Title 4',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/10/800/800',
+    price: 4000
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/13/800/800',
+    price: 5000
+  },
+  {
+    id: 6,
+    title: 'Title 6',
+    description: 'This is a production',
+    url: 'https://picsum.photos/id/15/800/800',
+    price: 6000
+  }
+]
 </script>
 
 <template>
   <MainLayout>
-    <div id="ShoppingCartPage" class="mt-4 max-w-[120px] mx-auto px-2">
-      <div v-if="true" class="h-[500px] flex items-center justify-center">
+    <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div v-if="false" class="h-[500px] flex items-center justify-center">
         <div class="pt-20">
           <img
             class="mx-auto"
@@ -18,11 +63,44 @@ import MainLayout from '~/layouts/MainLayout.vue'
           </div>
 
           <div v-if="true" class="flex text-center">
+            <NuxtLink
+              to="/auth"
+              class="bg-[#FD374F] w-full text-white text-[21px] font-semibold
+                p-1.5 rounded-full mt-4"
+            >
+              Sign in
+            </NuxtLink>
 
           </div>
 
         </div>
       </div>
+
+      <div
+        v-else
+        class="md:flex gap-4 justify-between mx-auto w-full"
+      >
+        <div class="md:w-[65%]">
+          <div class="bg-white rounded-lg p-4">
+            <div class="text-2xl font-bold mb-2">
+              Shopping Cart (0)
+            </div>
+          </div>
+
+          <div class="bg-[#FEEEEF] rounded-lg p-4 mt-4">
+            <div class="text-red-500 font-bold">
+              Welcome Deal applicable on 1 item only
+            </div>
+          </div>
+
+          <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+            <div v-for="product in products">
+
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </MainLayout>
 </template>
