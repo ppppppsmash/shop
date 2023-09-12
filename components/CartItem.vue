082dfaa20e4cac4c3038583f1bcce5506a64102d
ppppppsmash <pppppp.work@gmail.com>
2023-09-12 21:28:31 +0900

    mod: CartItem markup & clickイベント

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -44,5 +44,55 @@ watch(() => isSelected.value, (val) => {
         </div>
       </div>
     </div>
+
+    <img
+      class="rounded-md md:w-[150px] w-[90px]"
+      :src="product.url"
+    />
+
+    <div class="overflow-hidden pl-2 w-full">
+      <div class="flex items-center justify-between w-full">
+        <div class="flex items-center justify-between truncate">
+          <span class="sm:block hidden bg-[#FD374F] text-white text-[9px]
+            font-semibold px-1.5 rounded-sm min-w-[80px]">
+            Welcome Deal
+          </span>
+
+          <div class="truncate sm:pl-2">
+            {{ product.title }}
+          </div>
+        </div>
+
+        <button
+          @click="removeFromCart()"
+          class="mx-3 sm:block hidden -mt-0.5 hover:text-red-500"
+        >
+          <Icon name="material-symbols:delete-outline" size="20" />
+        </button>
+      </div>
+
+      <div class="text-xl font-semibold">
+        $ <span class="font-bold">{{ product.price / 100 }}</span>
+      </div>
+
+      <p class="text-[#009A66] text-xs font-semibold pt-1">
+        Free 11-day delivery over 8.28
+      </p>
+
+      <p class="text-[#009A66] text-xs font-semibold pt-1">
+        Free Shipping
+      </p>
+
+      <div class="flex items-center justify-end">
+        <button
+          @click="removeFromCart()"
+          class="sm:hidden block -mt-0.5 hover:text-red-500"
+        >
+          <Icon name="material-symbols:delete-outline" size="20" />
+        </button>
+      </div>
+
+    </div>
+
   </div>
 </template>
