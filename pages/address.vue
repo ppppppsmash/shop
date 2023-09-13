8bc73acf6bac6bde26d7f64f5353d9c99306f705
ppppppsmash <pppppp.work@gmail.com>
2023-09-13 22:20:34 +0900

    mod: addressページ

---
+++ pages/address.vue
@@ -0,0 +1,26 @@
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue'
+</script>
+
+<template>
+  <MainLayout>
+    <div
+      id="AddressPage"
+      class="mt-4 max-w-[500px] mx-auto px-2"
+    >
+      <div class="mx-auto bg-white rounded-lg p-3">
+        <div class="text-xl text-bold mb-2">Address Details</div>
+
+        <form @submit.prevent="submit()">
+          <TextInput
+            class="w-full"
+            placeholder="Contact Name"
+            v-model:input="contactName"
+            inputType="text"
+            :error="error && error.type == 'contactName' ? error.message : ''"
+          />
+        </form>
+      </div>
+    </div>
+  </MainLayout>
+</template>
