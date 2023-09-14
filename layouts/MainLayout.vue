a5f3e919052a6ba6256af45d13b3702154aef9ff
ppppppsmash <pppppp.work@gmail.com>
2023-09-14 22:50:03 +0900

    fix: メニューボタンイベント修正

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -186,7 +186,7 @@ let searchItem = ref('')
         </NuxtLink>
 
         <button
-          @clicl="userStore.isMenuOverlay = true"
+          @click="userStore.isMenuOverlay = true"
           class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
         >
           <Icon name="radix-icons:hamburger-menu" size="33" />
