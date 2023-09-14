e2181896d70861981d5273520bc9de055957b6e9
ppppppsmash <pppppp.work@gmail.com>
2023-09-14 22:24:10 +0900

    add: MenuOverlayコンポーネント

---
+++ components/MenuOverlay.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { useUserStore } from '~/stores/user'
+const userStore = useUserStore()
+
+// const client = useSupabaseClient()
+// const user = useSupabaseUser()
+
+const goTo = (url) => {
+  userStore.isMenuOverlay = false
+  return navigateTo(``)
+}
+
+const signOut = () => {
+  client.auth.signOut()
+  userStore.isMenuOverlay = false
+  return navigateTo('/')
+}
+
+const signIn = () => {
+  userStore.isMenuOverlay = false
+  return navigateTo('/auth')
+}
+</script>
+
+<template>
+  <div
+    id="MenuOverlay"
+    class="fixed z-50 bottom-0 h-full w-full bg-white px-3"
+  >
+    <div class="flex items-center justify-between py-5">
+      <NuxtLink
+        to="/"
+        @click="userStore.isMenuOverlay = false"
+      >
+        <img
+          width="170"
+          src="/AliExpress-logo.png"
+        />
+      </NuxtLink>
+
+      <button
+        @click="userStore.isMenuOverlay = false"
+        class="rounded-full p-1 hover:bg-gray-200"
+      >
+        <Icon name="mdi:close" size="30" />
+      </button>
+    </div>
+  </div>
+</template>
