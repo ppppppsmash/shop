b75ad296cf44cfea0a4469a4699b57b3c81d561e
ppppppsmash <pppppp.work@gmail.com>
2023-09-20 22:40:47 +0900

    add: prisma初期化(npx prisma init)

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ const route = useRoute()
 let windowWidth = ref(process.client ? window.innerWidth : '')
 
 onMounted(() => {
-  userStore.isLoading = true
+  userStore.isLoading = false
   window.addEventListener('resize', () => {
     windowWidth.value = window.innerWidth
   })
