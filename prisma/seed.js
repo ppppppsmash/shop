63b4277aedcd26d9016ad255758d3d194d07c601
ppppppsmash <pppppp.work@gmail.com>
2023-09-21 22:38:03 +0900

    mod: supabaseと連携、seedファイルを実行

--- prisma/seed.js
+++ prisma/seed.js
@@ -29,6 +29,43 @@ const seedProducts = async () => {
         price: 1900
       }
     })
+
+    await prisma.products.create({
+      data: {
+        title: 'コーヒーメーカー',
+        description: 'テキストテキストテキストテキストテキストテキストテキスト',
+        url: 'https://picsum.photos/id/19/800/800',
+        price: 1900
+      }
+    })
+
+    await prisma.products.create({
+      data: {
+        title: '冷蔵庫',
+        description: 'テキストテキストテキストテキストテキストテキストテキスト',
+        url: 'https://picsum.photos/id/18/800/800',
+        price: 3000
+      }
+    })
+
+    await prisma.products.create({
+      data: {
+        title: '掃除機',
+        description: 'テキストテキストテキストテキストテキストテキストテキスト',
+        url: 'https://picsum.photos/id/17/800/800',
+        price: 4000
+      }
+    })
+
+    await prisma.products.create({
+      data: {
+        title: '炊飯器',
+        description: 'テキストテキストテキストテキストテキストテキストテキスト',
+        url: 'https://picsum.photos/id/17/800/800',
+        price: 1500
+      }
+    })
+
   } catch(error) {
     console.error(error)
   } finally {
@@ -37,3 +74,11 @@ const seedProducts = async () => {
 }
 
 seedProducts()
+
+// npx prisma generate
+// npx prisma migrate dev --name init
+
+// supabase側のport番号は6543だが、postgres dbに合わせて、5432に変更
+// postgres://postgres.diyndubtrpbasvgvlpdj:[YOUR-PASSWORD]@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres
+
+// npx prisma db seed
