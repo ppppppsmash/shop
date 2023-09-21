6176fdc4ea65b785c8739ab108b191d09105c4e0
ppppppsmash <pppppp.work@gmail.com>
2023-09-21 23:12:16 +0900

    mod: 商品詳細画面でonBeforeMountとwatchEffectでsupabaseのdbと連携

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -5,13 +5,19 @@ const userStore = useUserStore()
 
 const route = useRoute()
 
+let product = ref(null)
 let currentImage = ref(null)
 
-onMounted(() => {
-  watchEffect(() => {
-    currentImage.value = 'https://picsum.photos/id/77/800/800'
-    images.value[0] = 'https://picsum.photos/id/77/800/800'
-  })
+onBeforeMount(async () => {
+  product.value = await useFetch(`/api/prisma/get-product-by-id/${route.params.id}`)
+})
+
+watchEffect(() => {
+  if (product.value && product.value.data) {
+    currentImage.value = product.value.data.url
+    images.value[0] = product.value.data.url
+    userStore.isLoading = false
+  }
 })
 
 const isInCart = computed(() => {
@@ -25,7 +31,11 @@ const isInCart = computed(() => {
 })
 
 const priceComputed = computed(() => {
-  return '26.40'
+  if (product.value && product.value.data) {
+    return product.value.data.price / 100
+  }
+
+  return '0.00'
 })
 
 const images = ref([
@@ -38,7 +48,7 @@ const images = ref([
 ])
 
 const addToCart = () => {
-  alert('ADDED')
+  userStore.cart.push(product.value.data)
 }
 </script>
 
@@ -71,9 +81,9 @@ const addToCart = () => {
         </div>
 
         <div class="md:w-[60%] bg-white p-3 rounded-lg">
-          <div v-if="true"> 
-            <p class="mb-2">Title </p>
-            <p class="font-light text-[12px] mb-2">Description Section</p>
+          <div v-if="product && product.data"> 
+            <p class="mb-2">{{ product.data.title }}</p>
+            <p class="font-light text-[12px] mb-2">{{ product.data.description }}</p>
           </div>
 
           <div class="flex items-center pt-1.5">
