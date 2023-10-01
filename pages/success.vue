75eace79d94c61d47ce66f207aeaf741a59727b2
ppppppsmash <pppppp.work@gmail.com>
2023-10-01 23:23:19 +0900

    add: 支払い成功時ページ

---
+++ pages/success.vue
@@ -0,0 +1,14 @@
+<script>
+import MainLayout from '~/layouts/MainLayout.vue'
+</script>
+
+<template>
+  <MainLayout>
+    <div
+      id="SuccessPage"
+      class="mt-4 max-w-[1200px] mx-auto px-2 min-h-[50vh]"
+    >
+
+    </div>
+  </MainLayout>
+</template>
